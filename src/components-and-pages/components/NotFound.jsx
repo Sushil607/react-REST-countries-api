aa32d0d5ae25bc6a1f3704711ba71976acdd99d5
Sushil607 <bodadesushil@gmail.com>
2023-06-16 18:28:21 +0530

    added NotFound component

--- src/components-and-pages/components/NotFound.jsx
+++ src/components-and-pages/components/NotFound.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React,{useContext} from 'react'
+
+import { ThemeContext } from '../../contexts/ThemeContext'
 
 const NotFound = () => {
+  const context = useContext(ThemeContext)
+  const darkTheme = context.darkTheme
   return (
-    <main className='main'>
+    <main className={`${darkTheme && 'dark'} main`}>
         <div className="container">
-            <div className="not-found">Not Found :(</div>
+            <div className="not-found">
+              <p>The content you are looking for is'nt available :(</p>
+              <p>Please go back to homepage.</p>
+            </div>
         </div>
     </main>
   )
